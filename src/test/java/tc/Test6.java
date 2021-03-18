26f3f97947197d8a076f5eff3019ed39ca89759c
QSP <QSP@DESKTOP-EVK9GMC>
2021-03-18 10:16:02 +0530

    TestNG

--- src/test/java/tc/Test6.java
+++ src/test/java/tc/Test6.java
@@ -1,38 +1,39 @@
 package tc;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class Testcase1 extends BaseClass{
-	
+public class Test6 {
+	public WebDriver driver;
+	@Parameters({"browserName"})
 	@Test
-	public void login() {
+	public void demo(String browser) {
+		if(browser.equals("chrome")) {
+			driver=new ChromeDriver();
+			}
+		else
+		{
+			driver=new FirefoxDriver();
+		}
+		driver.get("https://www.facebook.com/");
 		driver.findElement(By.linkText("Forgotten password?")).click();
 		driver.findElement(By.id("identify_email")).sendKeys("hjgfhgjh");
 		driver.findElement(By.id("did_submit")).click();
 		String title = driver.getTitle();
-		
-		//Assert.assertEquals(title,"34554ttrrtyert");
-		SoftAssert s=new SoftAssert();
-		s.assertEquals(title,"gjhgjhghg");
-				
+
 		Reporter.log(title, true);
 		String url = driver.getCurrentUrl();
 		Reporter.log(url,true);
 		
-		s.assertAll();
+		
 
 	}
-	
-
 
 }
